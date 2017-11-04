82bd769e18ee25d5bb37cf9177dad14ecf0a8833
Trung Nguyen <trungnguyen@anduintransact.com>
2017-11-04 18:42:17 +0700

    Add graphql-tools facades

---
+++ modules/apollo-graphql-tools/src/main/scala/anduin/scalajs/apollo/graphqltools/internal/ExecutableSchemaOptions.scala
@@ -0,0 +1,7 @@
+package anduin.scalajs.apollo.graphqltools.internal
+
+import scala.scalajs.js
+
+final class ExecutableSchemaOptions(
+  val typeDefs: String
+) extends js.Object
---
+++ modules/apollo-graphql-tools/src/main/scala/anduin/scalajs/apollo/graphqltools/internal/SchemaMockFunctionsOptions.scala
@@ -0,0 +1,7 @@
+package anduin.scalajs.apollo.graphqltools.internal
+
+import scala.scalajs.js
+
+final class SchemaMockFunctionsOptions(
+  val schema: js.Any
+) extends js.Object
