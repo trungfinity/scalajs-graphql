e75e03f1a50cb4fe13031264561ce05f9b22d63a
Trung Nguyen <trungnguyen@anduintransact.com>
2017-12-01 00:49:27 +0700

    Add some instances for encoder/decoder

--- modules/noton/noton-core/src/main/scala/anduin/scalajs/noton/Decoder.scala
+++ modules/noton/noton-core/src/main/scala/anduin/scalajs/noton/Decoder.scala
@@ -3,6 +3,7 @@
 package anduin.scalajs.noton
 
 import scala.scalajs.js
+import scala.util.Try
 
 trait Decoder[A] {
   def apply(any: js.Any): Decoder.Result[A]
@@ -12,19 +13,19 @@ object Decoder {
 
   type Result[A] = Either[Throwable, A]
 
-  def instance[A](f: js.Any => Result[A]): Decoder[A] = any => f(any)
+  def apply[A](implicit decoder: Decoder[A]): Decoder[A] = decoder
 
-  // scalastyle:off token
+  def instance[A](decode: js.Any => Result[A]): Decoder[A] = any => decode(any)
 
   implicit val intDecoder: Decoder[Int] = instance { any =>
-    Right(any.asInstanceOf[Int])
+    // Don't try to catch an UndefinedBehaviorError
+    Try(any.asInstanceOf[Int]).toEither // scalastyle:ignore token
   }
 
   implicit val stringDecoder: Decoder[String] = instance { any =>
-    Right(any.asInstanceOf[String])
+    // Don't try to catch an UndefinedBehaviorError
+    Try(any.asInstanceOf[String]).toEither // scalastyle:ignore token
   }
-
-  // scalastyle:on token
 
   implicit def optionDecoder[A](implicit decoder: Decoder[A]): Decoder[Option[A]] = {
     instance { any =>
@@ -32,6 +33,26 @@ object Decoder {
         decoder(any).map(Some(_))
       } else {
         Right(None)
+      }
+    }
+  }
+
+  implicit def listDecoder[A](implicit decoder: Decoder[A]): Decoder[List[A]] = {
+    instance { any =>
+      if (js.Array.isArray(any)) {
+        any
+          .asInstanceOf[js.Array[js.Any]] // scalastyle:ignore token
+          .reverse
+          .foldLeft(
+            Right(List.empty): Decoder.Result[List[A]]
+          ) { (listResult, elementAny) =>
+            for {
+              list <- listResult
+              element <- decoder(elementAny)
+            } yield element :: list
+          }
+      } else {
+        Left(new RuntimeException(s"$any is not an array."))
       }
     }
   }
