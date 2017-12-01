83cb24db83742c50455d6a5e02334377a366291d
Trung Nguyen <trungnguyen@anduintransact.com>
2017-12-02 02:47:31 +0700

    Complete plugin tests with common use cases

--- modules/codegen/sbt-codegen/src/sbt-test/sbt-graphql-codegen/01-generating-code/src/main/scala/anduin/graphql/example/Main.scala
+++ modules/codegen/sbt-codegen/src/sbt-test/sbt-graphql-codegen/01-generating-code/src/main/scala/anduin/graphql/example/Main.scala
@@ -4,16 +4,20 @@ package anduin.graphql.example
 
 import scala.scalajs.js
 
+import anduin.scalajs.noton.{Decoder, Encoder}
+
 object Main {
 
-  def main(args: Array[String]): Unit = {
-    val raw = js.Dynamic.literal(
-      "firstName" -> "Trung",
+  def testSomeoneQuery(): Unit = {
+    val firstNameJs = js.Dynamic.literal("value" -> "Trung")
+    val nameJs = js.Dynamic.literal(
+      "firstName" -> firstNameJs,
       "lastName" -> null
     )
 
-    val name = GetNameQuery.Data.Name("Trung", None)(raw)
-    val decodedName = GetNameQuery.Data.Name.decoder(raw)
+    val firstName = GetSomeoneNameAndAgeQuery.Data.Someone.Name.FirstName("Trung")(firstNameJs)
+    val name = GetSomeoneNameAndAgeQuery.Data.Someone.Name(firstName, None)(nameJs)
+    val decodedName = Decoder[GetSomeoneNameAndAgeQuery.Data.Someone.Name].apply(nameJs)
 
     println("Comparing between:")
     println(s"1. Original name: $name")
@@ -22,5 +26,102 @@ object Main {
     assert(decodedName == Right(name))
 
     println("Matched!")
+  }
+
+  def testPersonQuery(): Unit = {
+    val firstNameJs = js.Dynamic.literal("value" -> "Trung")
+    val nameJs = js.Dynamic.literal(
+      "firstName" -> firstNameJs,
+      "lastName" -> null
+    )
+
+    val firstName = GetPersonQuery.Data.Person.Name.FirstName("Trung")(firstNameJs)
+    val name = GetPersonQuery.Data.Person.Name(firstName, None)(nameJs)
+    val decodedName = Decoder[GetPersonQuery.Data.Person.Name].apply(nameJs)
+
+    println("Comparing between:")
+    println(s"1. Original name: $name")
+    println(s"2. Decoded name: $decodedName")
+
+    assert(decodedName == Right(name))
+
+    println("Matched!")
+
+    val variablesJs = js.Dynamic.literal("id" -> "13579")
+    val variables = GetPersonQuery.Variables("13579")
+    val encodedVariables = Encoder[GetPersonQuery.Variables].apply(variables)
+
+    println("Comparing between:")
+    println(s"1. Original variables: ${js.JSON.stringify(variablesJs)}")
+    println(s"2. Encoded variables: ${js.JSON.stringify(encodedVariables)}")
+
+    assert(js.JSON.stringify(variablesJs) == js.JSON.stringify(encodedVariables))
+
+    println("Matched")
+  }
+
+  def testPersonByNameQuery(): Unit = {
+    val firstNameInputJs = js.Dynamic.literal("value" -> "Trung")
+    val nameInputJs = js.Dynamic.literal(
+      "firstName" -> firstNameInputJs,
+      "lastName" -> "Nguyen"
+    )
+
+    val variablesJs = js.Dynamic.literal("name" -> nameInputJs)
+
+    val firstNameInput = FirstNameInput("Trung")
+    val nameInput = NameInput(firstNameInput, Some("Nguyen"))
+    val variables = GetPersonByNameQuery.Variables(nameInput)
+    val encodedVariables = Encoder[GetPersonByNameQuery.Variables].apply(variables)
+
+    println("Comparing between:")
+    println(s"1. Original variables: ${js.JSON.stringify(variablesJs)}")
+    println(s"2. Encoded variables: ${js.JSON.stringify(encodedVariables)}")
+
+    assert(js.JSON.stringify(variablesJs) == js.JSON.stringify(encodedVariables))
+
+    println("Matched")
+  }
+
+  def testPeopleByGenderQuery(): Unit = {
+    val genderJs = "MALE"
+    val gender = Gender.MALE
+    val encodedGender = Encoder[Gender].apply(gender)
+    val decodedGender = Decoder[Gender].apply(genderJs)
+
+    println("Comparing between:")
+    println(s"1. Original gender: ${js.JSON.stringify(genderJs)}")
+    println(s"2. Encoded gender: ${js.JSON.stringify(encodedGender)}")
+
+    assert(js.JSON.stringify(genderJs) == js.JSON.stringify(encodedGender))
+
+    println("Matched")
+
+    println("Comparing between:")
+    println(s"1. Original gender: $gender")
+    println(s"2. Decoded gender: $decodedGender")
+
+    assert(decodedGender == Right(gender))
+
+    println("Matched")
+
+    val variablesJs = js.Dynamic.literal("gender" -> genderJs)
+    val variables = GetPeopleByGenderQuery.Variables(gender)
+    val encodedVariables = Encoder[GetPeopleByGenderQuery.Variables].apply(variables)
+
+    println("Comparing between:")
+    println(s"1. Original variables: ${js.JSON.stringify(variablesJs)}")
+    println(s"2. Encoded variables: ${js.JSON.stringify(encodedVariables)}")
+
+    assert(js.JSON.stringify(variablesJs) == js.JSON.stringify(encodedVariables))
+
+    println("Matched")
+  }
+
+  def main(args: Array[String]): Unit = {
+    testSomeoneQuery()
+    testPersonQuery()
+    testPersonByNameQuery()
+    testPeopleByGenderQuery()
   }
 }
